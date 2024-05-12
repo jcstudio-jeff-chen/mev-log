63c36e07e7b511b27e01e090cca6f6c888582d12
jcstudio-jeff-chen <jcstudiojeffchen@gmail.com>
2024-05-12 13:20:35 +0800

    Add bat files

---
+++ lighthouse_mainnet_node_fast.bat
@@ -0,0 +1,10 @@
+E:\ethereum\consensus\lighthouse\lighthouse.exe bn ^
+  --network mainnet ^
+  --checkpoint-sync-url https://beaconstate.ethstaker.cc ^
+  --datadir E:\ethereum\consensus\lighthouse\mainnet ^
+  --execution-jwt E:\ethereum\execution\nethermind\keystore\jwt-secret ^
+  --execution-endpoint http://localhost:8551 ^
+  --builder http://localhost:18550 ^
+  --http ^
+  --gui
+pause
---
+++ lighthouse_mainnet_validator.bat
@@ -0,0 +1,8 @@
+E:\ethereum\consensus\lighthouse\lighthouse.exe vc ^
+  --network mainnet ^
+  --suggested-fee-recipient 0xC78c5Ce047A2FE6415915fFD910Ea1d41537879f ^
+  --datadir E:\ethereum\consensus\lighthouse\mainnet ^
+  --http ^
+  --builder-proposals ^
+  --graffiti "JC-Lab sent me here"
+pause
